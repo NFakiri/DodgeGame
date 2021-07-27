97fb7d14e83a21f57742619957f2618ff0869272
Nisar Fakiri <fakirinisar@gmail.com>
2021-07-27 14:43:19 +0100

    Created Game (main) class and Window class

---
+++ src/com/dodgegame/main/Game.java
@@ -0,0 +1,9 @@
+package com.dodgegame.main;
+
+public class Game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/com/dodgegame/main/Window.java
@@ -0,0 +1,9 @@
+package com.dodgegame.main;
+
+import java.awt.Canvas;
+
+public class Window extends Canvas{
+
+	private static final long serialVersionUID = 9027881205103034863L;
+	
+}
