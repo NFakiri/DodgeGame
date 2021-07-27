0faff9bb7cd3b2422b684ee583a8f0cb578f1f8e
Nisar Fakiri <fakirinisar@gmail.com>
2021-07-27 17:13:29 +0100

    Created functionality to allow the Window JFrame to be executed

--- src/com/dodgegame/main/Game.java
+++ src/com/dodgegame/main/Game.java
@@ -1,9 +1,29 @@
 package com.dodgegame.main;
 
-public class Game {
+import java.awt.Canvas;
 
+public class Game extends Canvas implements Runnable{
+
+	private static final long serialVersionUID = -4205364525572216790L;
+	
+	// Height and Width dimensions of the Window
+	public static final int WIDTH = 640, HEIGHT = WIDTH/12*9;
+
+	// Constructor method creating, creating new Window object.
+	public Game() {
+		new Window(WIDTH, HEIGHT, "Let's Build a Game!", this);
+	}
+	
+	public synchronized void start() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Game();
 	}
 
 }
