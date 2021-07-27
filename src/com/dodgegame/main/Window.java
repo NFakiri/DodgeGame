003bbfb7fe4e6430c3d3c29729186af3d76bf5ac
Nisar Fakiri <fakirinisar@gmail.com>
2021-07-27 16:39:14 +0100

    Added comments to Window class

--- src/com/dodgegame/main/Window.java
+++ src/com/dodgegame/main/Window.java
@@ -9,13 +9,17 @@ public class Window extends Canvas{
 
 	private static final long serialVersionUID = 9027881205103034863L;
 	
+	// Constructor that takes width, height, title and game object.
 	public Window(int width, int height, String title, Game game) {
+		// New JFrame object
 		JFrame frame = new JFrame(title);
 		
+		// Setting the size of the JFrame
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.setMaximumSize(new Dimension(width, height));
 		frame.setMinimumSize(new Dimension(width, height));
 		
+		// Set Frame attributes
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
