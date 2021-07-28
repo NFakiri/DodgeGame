a5022ab9242389d4c550958d4de3a4d32c79b172
Nisar Fakiri <fakirinisar@gmail.com>
2021-07-28 12:52:43 +0100

    Implemented abstract class GameObject and created classes

---
+++ src/com/dodgegame/main/ID.java
@@ -0,0 +1,7 @@
+package com.dodgegame.main;
+
+public enum ID {
+
+	Player(),
+	Enemy();
+}
---
+++ src/com/dodgegame/main/Player.java
@@ -0,0 +1,23 @@
+package com.dodgegame.main;
+
+import java.awt.Graphics;
+
+public class Player extends GameObject {
+
+	public Player(int x, int y, ID id) {
+		super(x, y, id);
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		
+	}
+	
+	
+
+}
