4e73f998030741aef3b197915bcf966ee0aeecd8
Nisar Fakiri <fakirinisar@gmail.com>
2021-07-29 17:56:06 +0100

    KeyInput class updated for Players using velocity. keyReleased() method implemented.

--- src/com/dodgegame/main/KeyInput.java
+++ src/com/dodgegame/main/KeyInput.java
@@ -19,16 +19,18 @@ public class KeyInput extends KeyAdapter{
 			
 			if(tempObject.getId() == ID.Player) {
 				// All key events for player 1.
-				
-				if (keyPressed == KeyEvent.VK_W) tempObject.setY(tempObject.getY() - 1);
-				if (keyPressed == KeyEvent.VK_S) tempObject.setY(tempObject.getY() + 1);
-				if (keyPressed == KeyEvent.VK_A) tempObject.setX(tempObject.getX() - 1);
-				if (keyPressed == KeyEvent.VK_D) tempObject.setX(tempObject.getX() + 1);
+				if (keyPressed == KeyEvent.VK_W) tempObject.setVelocityY(-5);
+				if (keyPressed == KeyEvent.VK_S) tempObject.setVelocityY(5);
+				if (keyPressed == KeyEvent.VK_A) tempObject.setVelocityX(-5);
+				if (keyPressed == KeyEvent.VK_D) tempObject.setVelocityX(5);
 			}
 			
 			if (tempObject.getId() == ID.Player2) {
 				// All key events for player 2
-				if (keyPressed == KeyEvent.VK_UP) tempObject.setY(tempObject.getY() - 1);
+				if (keyPressed == KeyEvent.VK_UP) tempObject.setVelocityY(-5);
+				if (keyPressed == KeyEvent.VK_DOWN) tempObject.setVelocityY(5);
+				if (keyPressed == KeyEvent.VK_LEFT) tempObject.setVelocityX(-5);
+				if (keyPressed == KeyEvent.VK_RIGHT) tempObject.setVelocityX(5);
 			}
 		}
 	}
@@ -36,5 +38,24 @@ public class KeyInput extends KeyAdapter{
 	public void keyReleased(KeyEvent e) {
 		int keyPressed = e.getKeyCode();
 		
+		for (int i = 0; i < handler.object.size(); i++) {
+			GameObject tempObject = handler.object.get(i);
+			
+			if(tempObject.getId() == ID.Player) {
+				// All key events for player 1.
+				if (keyPressed == KeyEvent.VK_W) tempObject.setVelocityY(0);
+				if (keyPressed == KeyEvent.VK_S) tempObject.setVelocityY(0);
+				if (keyPressed == KeyEvent.VK_A) tempObject.setVelocityX(0);
+				if (keyPressed == KeyEvent.VK_D) tempObject.setVelocityX(0);
+			}
+			
+			if (tempObject.getId() == ID.Player2) {
+				// All key events for player 2
+				if (keyPressed == KeyEvent.VK_UP) tempObject.setVelocityY(0);
+				if (keyPressed == KeyEvent.VK_DOWN) tempObject.setVelocityY(0);
+				if (keyPressed == KeyEvent.VK_LEFT) tempObject.setVelocityX(0);
+				if (keyPressed == KeyEvent.VK_RIGHT) tempObject.setVelocityX(0);
+			}
+		}
 	}
 }
