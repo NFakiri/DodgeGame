1fb582fc1c9fb02e6b19f68896d8511f43b3856d
Nisar Fakiri <fakirinisar@gmail.com>
2021-09-02 14:44:26 +0100

    Added random particles for menu aesthetics

--- src/com/dodgegame/main/Menu.java
+++ src/com/dodgegame/main/Menu.java
@@ -10,39 +10,39 @@ import java.util.Random;
 import com.dodgegame.main.Game.STATE;
 
 public class Menu extends MouseAdapter{
-	
+
 	private Game game;
 	private Handler handler;
 	private Random r = new Random();
-	
+
 	public Menu(Game game, Handler handler) {
 		this.game = game;
 		this.handler = handler;
 	}
-	
+
 	// play button
 	public void mousePressed(MouseEvent e) {
 		int mouseX = e.getX();
 		int mouseY = e.getY();
-		
+
 		if (game.gameState == STATE.Menu) {
 			if (mouseOver(mouseX, mouseY, 210, 150, 200, 64)) {
 				game.gameState = STATE.Game;
 				handler.addObject(new Player(Game.WIDTH/2-32, Game.HEIGHT/2-32, ID.Player, handler));
 				handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH - 32), r.nextInt(Game.HEIGHT - 52), ID.BasicEnemy, handler));
 			}
-			
+
 			// help button
 			if (mouseOver(mouseX, mouseY, 210, 250, 200, 64)) {
 				game.gameState = STATE.Help;
 			}
-			
+
 			// quit button
 			if (mouseOver(mouseX, mouseY, 210, 350, 200, 64)) {
 				System.exit(1);
 			}
 		}
-		
+
 		// back button for help
 		if (game.gameState == STATE.Help) {
 			if(mouseOver(mouseX, mouseY, 210, 350, 200, 64)) {
@@ -51,15 +51,15 @@ public class Menu extends MouseAdapter{
 			}
 		}
 	}
-	
+
 	public void mouseReleased(MouseEvent e) {
-		
+
 	}
-	
+
 	public void tick() {
 		
 	}
-	
+
 	private boolean mouseOver(int mouseX, int mouseY, int x, int y, int width, int height) {
 		if(mouseX > x && mouseX < x + width) {
 			if (mouseY > y  && mouseY < y + height) {
@@ -71,41 +71,41 @@ public class Menu extends MouseAdapter{
 			return false;
 		}
 	}
-	
+
 	public void render(Graphics g) {
 		if (game.gameState == STATE.Menu) {
 			Font font = new Font("arial", 1, 50);
 			Font font2 = new Font("arial", 1, 30);
-			
+
 			g.setFont(font);
 			g.setColor(Color.white);
 			g.drawString("Menu", 240, 70);
-			
+
 			g.setFont(font2);
 			g.drawRect(210, 150, 200, 64);
 			g.drawString("Play", 270, 190);
-			
+
 			g.drawRect(210, 250, 200, 64);
 			g.drawString("Help", 270, 290);
-			
+
 			g.drawRect(210, 350, 200, 64);
 			g.drawString("Quit", 270, 390);
 		} else if (game.gameState == STATE.Help) {
 			Font font = new Font("arial", 1, 50);
 			Font font2 = new Font("arial", 1, 30);
 			Font font3 = new Font("arial", 1, 20);
-			
+
 			g.setFont(font);
 			g.setColor(Color.white);
 			g.drawString("Help", 240, 70);
-			
+
 			g.setFont(font3);
 			g.drawString("Use WASD keys to move the player and dodge enemies!", 50, 200);
-			
+
 			g.setFont(font2);
 			g.drawRect(210, 350, 200, 64);
 			g.drawString("Back", 270, 390);
 		}
-	
+
 	}
 }
